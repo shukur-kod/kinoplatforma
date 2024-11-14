d2a81ac56ac02a31ed749699f74411991058226e
shukur-kod <dragulagraf@gmail.com>
2024-11-14 12:38:49 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+import Section from './Components/Section/Section';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/science-fiction" element={<Section title="Science Fiction" />} />
+          <Route path="/fantasy" element={<Section title="Fantasy" />} />
+          <Route path="/trending" element={<Section title="Trending" />} />
+          <Route path="/popular" element={<Section title="Popular" />} /> {/* Popular bo'limini qo'shish */}
+          <Route path="/" element={<Home />}/>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
